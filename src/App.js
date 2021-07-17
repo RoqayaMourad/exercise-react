de004b12693d5fd9f2953611c5c07875c37cea3c
roqaya299 <elephantsong29@example.com>
2021-07-17 16:47:56 +0200

    components types

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import Greet from "./Components/Greet";
+import Welcome from "./Components/Welcome";
+
+
+function App() {
+  return (
+    <div className="App">
+
+    <Greet />
+    <Welcome />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Welcome.js
@@ -0,0 +1,13 @@
+import React , {Component} from "react";
+
+// function Welcome (){
+
+// return <h1>Welcome</h1>;
+// }
+class Welcome extends Component {
+    render () {
+        return <h1>welcome</h1>
+    }
+}
+
+export default Welcome ;
