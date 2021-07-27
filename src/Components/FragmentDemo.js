a9a92a2575a2ea35740398699bab41587b4e93db
roqaya299 <elephantsong29@example.com>
2021-07-27 16:37:06 +0200

    lifecycle methods

---
+++ src/Components/FragmentDemo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FragmentDemo() {
+    return (
+        <div className="section">
+            <h2>Fragment Demo</h2>
+            <p>This descripes the Fragment </p>
+        </div>
+    )
+}
+
+export default FragmentDemo
